b5a4d80a74b45f8e03fa7542d2a78dbd63d81c03
Pascal Craponne <pascal@craponne.fr>
2015-07-31 23:02:35 +0200

    Added projects

---
+++ HappyDo.Job/Program.cs
@@ -0,0 +1,17 @@
+﻿#region HappyDo
+// This is HappyDo
+// which downloads files from one place to another
+// thus, making me happy.
+// https://github.com/picrap/HappyDo
+// MIT License
+#endregion
+
+namespace HappyDo.Job
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
